4a75e5371910d9637bd383106a1b14f325adb601
bholmesdev <hey@bholmes.dev>
2022-12-01 21:50:38 -0500

    feat: map to authors with async zod transform

--- src/pages/blog/_utils.ts
+++ src/pages/blog/_utils.ts
@@ -1,8 +1,7 @@
-import path from 'node:path';
-import { getAuthor } from '../../data/authors/index.js'
+import path from 'node:path'
 import type { BlogPost } from '../../types.js'
 
-export const slugFromFile = (file: string) => path.parse(file).name;
+export const slugFromFile = (file: string) => path.parse(file).name
 
 export function sortPosts(a: BlogPost, b: BlogPost) {
     return b.data.publishDate.getTime() - a.data.publishDate.getTime()
